be2c0bf0fa9ed7340e7a5e2f27f14627c2f7ee8d
pilpod <giacomo.corso@gmail.com>
2022-02-09 18:57:09 +0100

    prepareStatement in SQL query

--- src/main/java/com/jdbc/App.java
+++ src/main/java/com/jdbc/App.java
@@ -2,9 +2,9 @@ package com.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+
 
 /**
  * Hello world!
@@ -26,20 +26,11 @@ public final class App {
             Connection connection = DriverManager.getConnection(url, user, password);
             System.out.println("Connection Successfull");
 
-            Statement mysql = connection.createStatement();
-
-            // Deleting data in database
-            deleteDog(mysql);
-
-            // Adding data in database
-            System.out.println("Adding a Dog");
-            addDog(mysql);
-
-            // Update data in database
-            updateDog(mysql);
+            PreparedStatement mysql = connection.prepareStatement("SELECT * FROM dogs WHERE breed=?");
+            mysql.setString(1, "Pastor Aleman");
             
             // Retrieve all dogs
-            ResultSet data = mysql.executeQuery("SELECT * FROM dogs");
+            ResultSet data = mysql.executeQuery(); // If prepareStatement is to store,update or delete need use executeUpdate();
 
             while (data.next()) {
                 System.out.println(data.getString("name"));
@@ -51,17 +42,5 @@ public final class App {
             System.out.println("Connection Error: " + e.getMessage());
         }
 
-    }
-
-    public static void addDog(Statement mysql) throws SQLException {
-        mysql.executeUpdate("INSERT INTO dogs (name,breed) VALUES ('Bilbo','Pastor Aleman')");
-    }
-
-    public static void updateDog(Statement mysql) throws SQLException {
-        mysql.executeUpdate("UPDATE dogs SET name = 'SuperDog' WHERE name = 'Bilbo'");
-    }
-
-    public static void deleteDog(Statement mysql) throws SQLException {
-        mysql.executeUpdate("DELETE FROM dogs WHERE name = 'SuperDog'");
     }
 }
