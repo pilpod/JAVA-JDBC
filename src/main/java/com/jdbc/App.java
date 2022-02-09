552f1ca8c9443de19acdcc8af818d5abe8697b13
pilpod <giacomo.corso@gmail.com>
2022-02-09 13:49:46 +0100

    addded executeUpdate with query to insert a new dog

--- src/main/java/com/jdbc/App.java
+++ src/main/java/com/jdbc/App.java
@@ -19,13 +19,18 @@ public final class App {
 
             String table = "java_tests_jdbc";
 
-            String url = "jdbc:mysql://localhost:3306/" + table; // or jdbc:mysql://@localhost:3306/java_tests_jdbc
+            String url = "jdbc:mysql://localhost:3306/" + table + "?useSSL=false"; // or jdbc:mysql://@localhost:3306/java_tests_jdbc
             String user = "root";
             String password = "";
             Connection connection = DriverManager.getConnection(url, user, password);
             System.out.println("Connection Successfull");
 
             Statement mysql = connection.createStatement();
+
+            System.out.println("Adding a Dog");
+            mysql.executeUpdate("INSERT INTO dogs (name,breed) VALUES ('Max','Pastor Aleman')");
+            
+            // Retrieve all dogs
             ResultSet data = mysql.executeQuery("SELECT * FROM dogs");
 
             while (data.next()) {
